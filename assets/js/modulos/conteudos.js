a112c24325cf5c0187ab80567e5bb39515d39305
Gabries <105074547+gabrieszin@users.noreply.github.com>
2023-05-21 21:42:15 -0300

    Alterando conteúdo

---
+++ assets/js/modulos/conteudos.js
@@ -0,0 +1,87 @@
+const base_html = `
+<header class="header">
+<div class="header-main container">
+  <div class="header-main-content">
+    <img class="header-main-content-photo" src="./assets/img/profile.png">
+    <hgroup class="hgroup">
+      <h1 class="hgroup-principal">consumindo a API da Alura</h1>
+      <h2 class="hgroup-secondary">meus cursos e trilhas</h2>
+    </hgroup>
+  </div>
+  <div class="header-main-actions">
+    <a href="#" class="btn header-main-actions-link action" data-link="my-profile">meu perfil<span class="open-in-new"></span></a>
+  </div>
+</div>
+</header>
+
+<main class="main content">
+<div class="container">
+  <section class="info-group">
+    <div class="info-group-header">
+      <h2 class="info-group-header-title">meus cursos</h2>
+      <hr class="info-group-header-line">
+    </div>
+    <div class="info-group-body area-courses" data-area="area-courses">
+    </div>
+  </section>
+  
+  <section class="info-group">
+    <div class="info-group-header">
+      <h2 class="info-group-header-title">trilhas e formações</h2>
+      <hr class="info-group-header-line">
+    </div>
+    <div class="info-group-body area-formations" data-area="area-formations">
+    </div>
+  </section>
+</div>
+</main>
+
+<div class="footer container"> 
+<footer> 
+  <div class="row"> 
+    <div class="col-12 col-md"> 
+      <div>
+        <small class="d-block">Desenvolvido por</small> 
+        <a href="" data-link="github-dev">
+          <h5 class="bold">Gabriel Ribeiro</h5>
+        </a>
+      </div><br>
+      <div>
+        <small class="d-block">&copy; <span data-ano-atual=''>2023</span></small> 
+        <small class="d-block mb-3-muted">Todos os direitos reservados.</small> 
+      </div>
+    </div> 
+    <div class="col-6 col-md"> 
+      <h5>Alura</h5> 
+      <ul class="list-unstyled text-small"> 
+        <li class="mb-1"><a class="text-decoration-none" href="#" data-link="my-profile">Meu perfil</a></li> 
+        <li class="mb-1"><a class="text-decoration-none" href="#" data-link="profile-one">Perfil ONE</a></li> 
+      </ul> 
+    </div> 
+    <div class="col-6 col-md"> 
+      <h5>Outros projetos</h5>
+      <ul class="list-unstyled text-small"> 
+        <li class="mb-1"><a class="text-decoration-none" href="#" data-link="expresso-mobilidade">Expresso Mobilidade</a></li> 
+        <li class="mb-1"><a class="text-decoration-none" href="#" data-link="music-ui">music.ui</a></li> 
+        <li class="mb-1"><a class="text-decoration-none" href="#" data-link="confirmacao-cca">Confirmação CCA</a></li> 
+        <li class="mb-1"><a class="text-decoration-none" href="#" data-link="birthday-message">Birthday Message</a></li> 
+      </ul> 
+    </div> 
+    <div class="col-6 col-md"> 
+      <h5>Sobre</h5>
+      <ul class="list-unstyled text-small"> 
+        <li class="mb-1"><a class="text-decoration-none" href="#" data-link="github-dev">Desenvolvedor</a></li> 
+        <li class="mb-1"><a class="text-decoration-none" href="#" data-link="github-projeto">GitHub</a></li> 
+        <li class="mb-1"><a class="text-decoration-none" href="#" data-link="portfolio">Portfólio</a></li> 
+        <li class="mb-1"><a class="text-decoration-none" href="#" data-link="linkedin">Linkedin</a></li> 
+      </ul> 
+    </div> 
+  </div> 
+</footer>
+<br> 
+</div>
+`;
+
+export{
+  base_html
+}
--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,7 @@
 
 import { Cards } from "./modulos/cards.js";
 import { isEmpty, atribuirLinks } from "./modulos/utilitarios.js";
+import { base_html } from "./modulos/conteudos.js";
 
 (() => {
   
@@ -20,95 +21,98 @@ import { isEmpty, atribuirLinks } from "./modulos/utilitarios.js";
       window.location.reload();
     })
   })
+  
+  document.querySelector('body').innerHTML += base_html;
 
   window.addEventListener('load', () => {
     setTimeout(() => {
       document.querySelector('[data-element="loading"]').classList.add('slide-out-top');
+      document.querySelector('html').classList.remove('block-scroll');
+    }, 500);
+    
+    const area_courses = document.querySelector('[data-area="area-courses"]');
+    const area_formations = document.querySelector('[data-area="area-formations"]');
+    
+    area_courses.innerHTML += new Cards('courses placeholder', null).createCard()
+    area_formations.innerHTML += new Cards('formations placeholder', null).createCard()
+    
+    const requireAPI = (element_type) => {
+      async function conexao(){
+        try{
+          const url = `https://www.alura.com.br/api/dashboard/d217e36b4071d6c4d6e6e97c4ced902a48332f60a59705954cc11c570724ba78`;
+          return await fetch(url)
+          .then((response) => 
+          response.json()
+          )
+          .then((retorno) => 
+          retorno
+          )
+          .catch((error) => 
+          error
+          )
+        }catch(error){
+          console.log(error);
+        }
+      }
+      
+      conexao().then(retorno => {
+        area_courses.innerHTML = '';
+        area_formations.innerHTML = '';
+        
+        //Caso existam dados para os cursos sendo realizados
+        if(!isEmpty(retorno.courseProgresses)){
+          const dados = Array.of(retorno.courseProgresses);
+          
+          dados[0].forEach(course => {
+            const card = new Cards('course', course.name.trim(), null, {percentage: course.progress}, {last_access: course.lastAccessTime})
+            area_courses.innerHTML += card.createCard();
+          })
+        }else{
+          //Caso não existam dados para os cursos em realização
+          area_courses.innerHTML += new Cards('courses empty', null).createCard();
+        }
+        
+        //Caso existam dados para as formações
+        if(!isEmpty(retorno.guides)){
+          const dados = Array.of(retorno.guides);
+          
+          dados[0].forEach(formation => {
+            const card = new Cards('formation', formation.name, {color: formation.color}, {finishedSteps: formation.finishedSteps, totalSteps: formation.totalSteps, finishedCourses: formation.finishedCourses}, {last_access: formation.lastAccessTime});
+            area_formations.innerHTML += card.createCard();
+          })
+        }
+        else{
+          //Caso não existam dados para as formações
+          area_formations.innerHTML += new Cards('formations empty', null).createCard();
+        }
+        
+        renderTooltips();
+        listernerClickRefreshQueryAPI();
+      });
+    }
+    
+    const clickRefreshQueryAPI = (evento) => {
+      const element_type = evento.target.closest('[data-area]').dataset.area.split('-')[1];
+      evento.target.closest('[data-area]').innerHTML = new Cards(`${element_type} placeholder`, null).createCard();
+      setTimeout(() => {
+        requireAPI(element_type);
+      }, 1000)
+    }
+    
+    const listernerClickRefreshQueryAPI = () => {
+      document.querySelectorAll('[data-action="refresh-query-api"]').forEach(button => {
+        button.addEventListener('click', (evento) => {
+          clickRefreshQueryAPI(evento);
+        })
+      })
+    }
+    
+    atribuirLinks();
+    
+    setTimeout(() => {
+      requireAPI();
+      listernerClickRefreshQueryAPI();
     }, 1000);
   })
   
-  const area_courses = document.querySelector('[data-area="area-courses"]');
-  const area_formations = document.querySelector('[data-area="area-formations"]');
-  
-  area_courses.innerHTML += new Cards('courses placeholder', null).createCard()
-  area_formations.innerHTML += new Cards('formations placeholder', null).createCard()
-  
-  const requireAPI = (element_type) => {
-    async function conexao(){
-      try{
-        const url = `https://www.alura.com.br/api/dashboard/d217e36b4071d6c4d6e6e97c4ced902a48332f60a59705954cc11c570724ba78`;
-        return await fetch(url)
-        .then((response) => 
-        response.json()
-        )
-        .then((retorno) => 
-        retorno
-        )
-        .catch((error) => 
-        error
-        )
-      }catch(error){
-        console.log(error);
-      }
-    }
-    
-    conexao().then(retorno => {
-      area_courses.innerHTML = '';
-      area_formations.innerHTML = '';
-
-      //Caso existam dados para os cursos sendo realizados
-      if(!isEmpty(retorno.courseProgresses)){
-        const dados = Array.of(retorno.courseProgresses);
-        
-        dados[0].forEach(course => {
-          const card = new Cards('course', course.name.trim(), null, {percentage: course.progress}, {last_access: course.lastAccessTime})
-          area_courses.innerHTML += card.createCard();
-        })
-      }else{
-        //Caso não existam dados para os cursos em realização
-        area_courses.innerHTML += new Cards('courses empty', null).createCard();
-      }
-      
-      //Caso existam dados para as formações
-      if(!isEmpty(retorno.guides)){
-        const dados = Array.of(retorno.guides);
-        
-        dados[0].forEach(formation => {
-          const card = new Cards('formation', formation.name, {color: formation.color}, {finishedSteps: formation.finishedSteps, totalSteps: formation.totalSteps, finishedCourses: formation.finishedCourses}, {last_access: formation.lastAccessTime});
-          area_formations.innerHTML += card.createCard();
-        })
-      }
-      else{
-        //Caso não existam dados para as formações
-        area_formations.innerHTML += new Cards('formations empty', null).createCard();
-      }
-      
-      renderTooltips();
-      listernerClickRefreshQueryAPI();
-    });
-  }
-
-  const clickRefreshQueryAPI = (evento) => {
-    const element_type = evento.target.closest('[data-area]').dataset.area.split('-')[1];
-    evento.target.closest('[data-area]').innerHTML = new Cards(`${element_type} placeholder`, null).createCard();
-    setTimeout(() => {
-      requireAPI(element_type);
-    }, 1000)
-  }
-
-  const listernerClickRefreshQueryAPI = () => {
-    document.querySelectorAll('[data-action="refresh-query-api"]').forEach(button => {
-      button.addEventListener('click', (evento) => {
-        clickRefreshQueryAPI(evento);
-      })
-    })
-  }
-  
-  atribuirLinks();
-  
-  setTimeout(() => {
-    requireAPI();
-    listernerClickRefreshQueryAPI();
-  }, 2000);
-
 })();
